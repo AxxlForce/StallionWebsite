a928b4992e70fa58b81aec08cc55616dc2f2dc7b
alex <eetfuk1988@gmail.com>
2013-09-12 19:07:20 +0200

    improved layout responsive design, improved code modularity

--- GetTheMuthafuckinEP/index.php
+++ GetTheMuthafuckinEP/index.php
@@ -5,11 +5,11 @@
     <!--font-->
     <link href='http://fonts.googleapis.com/css?family=Stardos+Stencil:400,700' rel='stylesheet' type='text/css'>
 
+    <!--jQuery-->
+    <script src="../ExternalFrameworks/jquery/jquery-2.0.1.min.js"></script>
+
     <!--define viewport for responsiveness-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-
-    <!--jQuery-->
-    <script src="ExternalFrameworks/jquery/jquery-2.0.1.min.js"></script>
 
     <!--bootstrap-->
     <link rel="stylesheet" href="../ExternalFrameworks/bootstrap/css/bootstrap.css">
@@ -21,73 +21,96 @@
 
     <!--submit script-->
     <script type="text/javascript">
-        function submitform()
+
+        function submitForm()
         {
-            document.myform.submit();
+            document.myForm.submit();
         }
+
+        function resizeHandler()
+        {
+            var borderDivArray = $(".borders");
+            var screenWidth = $(window).width();
+
+            for (var i = 0; i < borderDivArray.size(); i++)
+            {
+                if (screenWidth > 767)
+                {
+                    $(borderDivArray[i]).height(screenWidth / 105);
+                }
+                else
+                {
+                    $(borderDivArray[i]).height(0);
+                }
+            }
+        }
+
+        $(document).ready(resizeHandler);
+        $(window).resize(resizeHandler);
+
     </script>
-
-    <!--read file php function-->
-    <?php function readFromFile()
-    {
-        $filename = "counter.dat";
-
-        $handle = fopen($filename, "r");
-        $contents = fread($handle, filesize($filename));
-        fclose($handle);
-        return $contents;
-    }?>
 
 </head>
 <body>
 
-<div class="container">
+<div class="container-fluid">
 
     <!--upper column-->
-    <div class="row">
-        <div style="background-color: rgb(255,0,25); height: 30px">
-        </div>
+    <div class="row-fluid">
+        <div class="borders" style="background-color: white"/>
+    </div>
+    <div class="row-fluid">
+        <div class="borders" style="background-color: white"/>
+    </div>
+    <div class="row-fluid">
+        <div class="borders" style="background-color: rgb(255,0,25)"/>
     </div>
 
     <!--content column-->
-    <div class="row" style="overflow: hidden">
+    <div class="row-fluid" style="overflow: hidden">
 
-        <!--download span-->
-        <div class="span4 col">
+        <!--download span, hidden on mobile-->
+        <div class="span4 col hidden-phone">
 
             <div style="padding-top: 50%">
 
-                <h2><a href="javascript: submitform()"><u>Download</u></a> it, eh?!</h2>
+                <h2 align="center"><a href="javascript: submitForm()"><u>Download</u></a> it, eh?!</h2>
 
             </div>
         </div>
 
         <!--image span-->
         <div class="span4 col">
-            <div style="padding: 10px">
-                <a href="javascript: submitform()"><img src="img/pic_600x694.jpg"></a>
+            <div style="padding: 7%">
+
+                <a href="javascript: submitForm()"><img src="img/pic_600x694.jpg"></a>
+
+                <!--label for mobile layout-->
+                <h2 class="visible-phone" align="center"><a href="javascript: submitForm()"><u>Download</u></a> it, eh?!
+                </h2>
+
             </div>
         </div>
 
-        <!--counter span-->
-        <div class="span4 col">
+        <!--counter span, hidden on mobile-->
+        <div class="span4 col hidden-phone">
 
             <div style="padding-top: 50%">
 
-                <h2>Total Downloads: <?php echo readFromFile()?></h2>
+                <h2 align="center">Total Downloads: <?php include 'fileIO.php'; echo readFromFile()?></h2>
 
             </div>
         </div>
     </div>
 
     <!--lower column-->
-    <div class="row">
-        <div style="background-color: rgb(255,0,25); height: 30px">
-        </div>
+    <div class="row-fluid">
+        <div class="borders" style="background-color: rgb(255,0,25)"/>
     </div>
 
-    <form name="myform" method="POST" action="script.php">
+    <form name="myForm" method="POST" action="script.php">
         <input type="hidden" name="nw_update" value=""/>
+    </form>
 
 </div>
 
--- GetTheMuthafuckinEP/script.php
+++ GetTheMuthafuckinEP/script.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'fileIO.php';
+
 /*
 * check post
 */
@@ -14,85 +16,13 @@ if(isset($_POST['nw_update']))
 
 function incrementCounter() 
 {
-	// $value = readValue();
-	// writeValue(1);
-
 	$incrementedValue = readFromFile() + 1;
 	writeToFile($incrementedValue);
-}
-
-function readFromFile()
-{
-	$filename = "counter.dat";
-
-	$handle = fopen($filename, "r");
-	$contents = fread($handle, filesize($filename));
-	fclose($handle);
-	return $contents;
-}
-
-function writeToFile($value)
-{
-	$filename = "counter.dat";
-
-	if (is_writable($filename)) 
-	{
-		if (!$handle = fopen($filename, "w+")) 
-		{
-			exit;
-		}
-		if (!fwrite($handle, $value)) 
-		{
-			exit;
-		}
-
-		fclose($handle);
-	} 
 }
 
 function redirect($url, $statusCode = 303)
 {
 	header('Location: ' . $url, true, $statusCode);
 }
-
-// function readValue() 
-// {
-// 	$connection = mysql_connect("db4free.net","","");
-
-// 	// Check connection
-// 	if ( $connection )
-// 	{
-// 		$db = mysql_select_db( "stalliondb" );
-
-// 		if( $db )
-// 		{
-// 			$result = mysql_query("SELECT Counter FROM DownloadCounter");
-
-// 			if ( $result ) 
-// 			{
-// 				$row = mysql_fetch_row($result);
-// 				return $row[0];
-// 			}
-// 		}
-// 	}
-// 	mysql_close();
-// }
-
-// function writeValue($value) 
-// {
-// 	$connection = mysql_connect("db4free.net","","");
-
-// 		// Check connection
-// 	if ( $connection )
-// 	{
-// 		$db = mysql_select_db( "stalliondb" );
-
-// 		if( $db )
-// 		{
-// 			$result = mysql_query(" UPDATE DownloadCounter SET Counter=" . $value);
-// 		}
-// 	}
-// 	mysql_close();
-// }
 
 ?>
